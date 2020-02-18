40e31b45a801d7d5ad15f3ed59fe17abc3cd005b
Eddy Ahlqvist <eddy.ahlqvist@gmail.com>
2020-02-18 19:42:46 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,4 +6,24 @@ function setup() {
 
 function draw() {
   background(0);
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+  function addZero() {
+    if (sc < 10) {
+      sc = "0" + sc;
+    }
+    
+    if (mn < 10) {
+      mn = "0" + mn;
+    }
+  }
+  
+  addZero();
+
+  fill(255);
+  text(hr, 60, 100);
+  text(mn, 80, 100);
+  text(sc, 100, 100);
 }
